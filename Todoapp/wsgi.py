33f9084a9d547ee6f5bbf51024943d99b638a1a5
Ajakaye <mailpaulade@gmail.com>
2021-01-08 14:27:47 +0100

    pushed changes

--- Todoapp/wsgi.py
+++ Todoapp/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise import WhiteNoise
+
 
 
 
